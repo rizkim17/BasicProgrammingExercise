515030b546a594f46a73de4346b8d61ce18eb7a1
rizkim17 <160828490+rizkim17@users.noreply.github.com>
2024-03-07 14:07:22 +0700

    Delete session3 directory
    kaciri ges

--- session3/1_assignment_reverse_name.py
+++
@@ -1,5 +0,0 @@
-#Create a Python program that takes a user's name as input and prints it in reverse.
-
-name = input("Please input your name here: ") [::-1] #reverse
-
-print(name)
--- session3/7_assignment_string_length.py
+++
@@ -1,9 +0,0 @@
-#Take a string as input and print its length.
-
-#let user input the string
-string = str(input("Input a string to check the length: "))
-#count the length of the string
-length = len(string)
-
-#print the result
-print(f"String value '{string}' has a length of {length}")
